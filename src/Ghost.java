d5b0f2fb4c98b77166e4408ac1518eca59582c5f
Romain_Gojard <romain.gojard@gmail.com>
2022-07-01 18:42:06 +0200

    Strategy class

--- src/Ghost.java
+++ src/Ghost.java
@@ -5,82 +5,41 @@ public class Ghost extends Monster {
 
     private static int counter = 0;
 
-    private String role;
+    private Strategy strategy;
 
     public Ghost(int i, int j, Terrain terrain) {
         super(i, j, terrain);
         switch (counter) {
             case 0 -> {
                 color = Color.ANSI_RED;
-                role = "follow";
+                strategy = new Strategy("follow");
             }
             case 1 -> {
                 color = Color.ANSI_PURPLE;
-                role = "follow";
+                strategy = new Strategy("follow");
             }
             case 2 -> {
                 color = Color.ANSI_GREEN;
-                role = "random";
+                strategy = new Strategy("random");
             }
             case 3 -> {
                 color = Color.ANSI_CYAN;
-                role = "random";
+                strategy = new Strategy("random");
             }
         }
         forms = new char[]{'⋐', '⋑', '⋒', '⋓'};
         form = forms[0];
-        counter ++;
+        counter++;
     }
 
     public String whereToGo() {
-        PacMan pacMan = terrain.getPacMan();
-        String direction = "";
-        int[] ghostCoord = this.getCoordinates();
-        switch (this.role) {
-            case "follow" -> {
-                int[] pacManCoord = pacMan.getCoordinates();
-                int[] vector = {ghostCoord[0] - pacManCoord[0], ghostCoord[1] - pacManCoord[1]};
-                boolean isXDistanceBigger = Math.abs(vector[0]) >= Math.abs(vector[1]);
-                if (isXDistanceBigger & (vector[0] > 0) & terrain.getIsTroughable(ghostCoord[0] - 1, ghostCoord[1])) {
-                    direction = "up";
-                } else if (isXDistanceBigger & (vector[0] < 0) & terrain.getIsTroughable(ghostCoord[0] + 1, ghostCoord[1])) {
-                    direction = "down";
-                } else {
-                    if (vector[1] > 0 & terrain.getIsTroughable(ghostCoord[0], ghostCoord[1] - 1)) {
-                        direction = "left";
-                    } else if (vector[1] <= 0 & terrain.getIsTroughable(ghostCoord[0], ghostCoord[1] + 1)) {
-                        direction = "right";
-                    } else if (vector[0] > 0 & terrain.getIsTroughable(ghostCoord[0], ghostCoord[1])) {
-                        direction = "up";
-                    } else {
-                        direction = "down";
-                    }
-                }
-            }
-            case "random" -> {
-                boolean good = false;
-                int r = new Random().nextInt(4);
-                ArrayList<String> tabOfDirections = new ArrayList<>();
-                tabOfDirections.add("left");
-                tabOfDirections.add("up");
-                tabOfDirections.add("right");
-                tabOfDirections.add("down");
-                direction = tabOfDirections.get(r);
-                while (!good) {
-                    switch (direction) {
-                        case "left" -> good = terrain.getIsTroughable(ghostCoord[0], ghostCoord[1] - 1);
-                        case "up" -> good = terrain.getIsTroughable(ghostCoord[0] - 1, ghostCoord[1]);
-                        case "right" -> good = terrain.getIsTroughable(ghostCoord[0], ghostCoord[1] + 1);
-                        case "down" -> good = terrain.getIsTroughable(ghostCoord[0] + 1, ghostCoord[1]);
-                    }
-                    if (!good) {
-                        tabOfDirections.remove(r);
-                        r = new Random().nextInt(tabOfDirections.size());
-                        direction = tabOfDirections.get(r);
-                    }
-                }
-            }
+        String direction;
+        switch (strategy.getName()) {
+            case ("follow") -> direction = strategy.follow(terrain, this);
+            case ("random") -> direction = strategy.random(terrain, this);
+            default -> direction = null;
         }
         return direction;
     }
+
 }
---
+++ src/Strategy.java
@@ -0,0 +1,67 @@
+import java.util.ArrayList;
+import java.util.Random;
+
+public class Strategy {
+
+    private final String name;
+
+    public Strategy(String name){
+        this.name = name;
+    }
+
+    public String follow(Terrain terrain, Ghost ghost) {
+        PacMan pacMan = terrain.getPacMan();
+        String direction;
+        int[] ghostCoord = ghost.getCoordinates();
+        int[] pacManCoord = pacMan.getCoordinates();
+        int[] vector = {ghostCoord[0] - pacManCoord[0], ghostCoord[1] - pacManCoord[1]};
+        boolean isXDistanceBigger = Math.abs(vector[0]) >= Math.abs(vector[1]);
+        if (isXDistanceBigger & (vector[0] > 0) & terrain.getIsTroughable(ghostCoord[0] - 1, ghostCoord[1])) {
+            direction = "up";
+        } else if (isXDistanceBigger & (vector[0] < 0) & terrain.getIsTroughable(ghostCoord[0] + 1, ghostCoord[1])) {
+            direction = "down";
+        } else {
+            if (vector[1] > 0 & terrain.getIsTroughable(ghostCoord[0], ghostCoord[1] - 1)) {
+                direction = "left";
+            } else if (vector[1] <= 0 & terrain.getIsTroughable(ghostCoord[0], ghostCoord[1] + 1)) {
+                direction = "right";
+            } else if (vector[0] > 0 & terrain.getIsTroughable(ghostCoord[0], ghostCoord[1])) {
+                direction = "up";
+            } else {
+                direction = "down";
+            }
+        }
+        return direction;
+    }
+
+    public String random(Terrain terrain, Ghost ghost) {
+        String direction;
+        int[] ghostCoord = ghost.getCoordinates();
+        boolean good = false;
+        int r = new Random().nextInt(4);
+        ArrayList<String> tabOfDirections = new ArrayList<>();
+        tabOfDirections.add("left");
+        tabOfDirections.add("up");
+        tabOfDirections.add("right");
+        tabOfDirections.add("down");
+        direction = tabOfDirections.get(r);
+        while (!good) {
+            switch (direction) {
+                case "left" -> good = terrain.getIsTroughable(ghostCoord[0], ghostCoord[1] - 1);
+                case "up" -> good = terrain.getIsTroughable(ghostCoord[0] - 1, ghostCoord[1]);
+                case "right" -> good = terrain.getIsTroughable(ghostCoord[0], ghostCoord[1] + 1);
+                case "down" -> good = terrain.getIsTroughable(ghostCoord[0] + 1, ghostCoord[1]);
+            }
+            if (!good) {
+                tabOfDirections.remove(r);
+                r = new Random().nextInt(tabOfDirections.size());
+                direction = tabOfDirections.get(r);
+            }
+        }
+        return direction;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
